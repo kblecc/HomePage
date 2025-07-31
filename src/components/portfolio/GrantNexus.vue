3319ec6220ec564864a17327661d701af6a23ab9
kble <github@kble.cc>
2025-08-01 03:46:13 +0800

    Add: portfolio - grantnexus

---
+++ src/components/portfolio/GrantNexus.vue
@@ -0,0 +1,5 @@
+<template>
+<img class="img-fluid" src="https://raw.githubusercontent.com/kblecc/StaticAsserts/refs/heads/HomePage/portfolio/GrantNexus/Mood Board v1.jpg">
+</template>
+<style scoped>
+</style>
