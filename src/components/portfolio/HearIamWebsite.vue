0dcaf5608a7711a04be45c8318abfce802d10a9e
kble <github@kble.cc>
2025-08-06 08:45:42 +0800

    update: hear i am content

--- src/components/portfolio/HearIamWebsite.vue
+++ src/components/portfolio/HearIamWebsite.vue
@@ -5,15 +5,17 @@
     alt="Scenic nature landscape with mountains, lake, and forest during golden hour"
   />
   <div class="content">
-    <!-- <h3 class="mb-1 fw-bold">Grant Nexus</h3>
-    <div class="k-tags-container">
-      <span class="badge rounded-pill text-bg-primary">Design</span>
-      <span class="badge rounded-pill text-bg-primary">UI</span>
-    </div>
 
-    <p class="mb-0 text-muted">
-      Hear i am is a mobie application for music therapy combined with mental care
-    </p> -->
+      <h3 class="mb-0 fw-bold">Hear i am Website</h3>
+      <div class="k-tags-container">
+        <span class="badge rounded-pill text-bg-primary">Design</span
+        ><span class="badge rounded-pill text-bg-primary">Development</span
+        ><span class="badge rounded-pill text-bg-primary">Website</span
+        ><span class="badge rounded-pill text-bg-primary">Graphic</span>
+      </div>
+      <p class="mb-0 text-muted">
+        Website reamp for Hear i am application with new design and features
+      </p>
 
     <img
       class="k-content-image"
