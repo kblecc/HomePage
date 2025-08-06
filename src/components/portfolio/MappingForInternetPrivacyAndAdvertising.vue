2c38ea61123e4cbc70b8092a643f2bb0dfa8ac87
kble <github@kble.cc>
2025-08-06 08:21:48 +0800

    fix: cover image, update: intro

--- src/components/portfolio/MappingForInternetPrivacyAndAdvertising.vue
+++ src/components/portfolio/MappingForInternetPrivacyAndAdvertising.vue
@@ -4,10 +4,21 @@
     class="k-card-img"
     alt="Scenic nature landscape with mountains, lake, and forest during golden hour"
   />
+  <div class="content">
+    <h3 class="mb-1 fw-bold">Mapping for Internet Privacy & Advertising</h3>
+    <div class="k-tags-container">
+      <span class="badge rounded-pill text-bg-primary">Design</span>
+      <span class="badge rounded-pill text-bg-primary">UI</span>
+    </div>
+
+    <p class="mb-0 text-muted">
+      Graph of non-IT technicians to understand things behind a website
+    </p>
+
   <carousel
     :slides="[
        {
-        img: 'https://raw.githubusercontent.com/kblecc/HomePage/refs/heads/2025/src/assets/portfolio/MappingForInternetPrivacyAndAdvertising/cover.jpg',
+        img: 'https://raw.githubusercontent.com/kblecc/HomePage/refs/heads/2025/src/assets/portfolio/MappingForInternetPrivacyAndAdvertising/Cover.jpg',
       },
       {
         img: 'https://raw.githubusercontent.com/kblecc/HomePage/refs/heads/2025/src/assets/portfolio/MappingForInternetPrivacyAndAdvertising/p1.jpg',
@@ -26,6 +37,7 @@
       },
     ]"
   />
+  </div>
 </template>
 <script>
 import carousel from '../carousel.vue'
@@ -41,5 +53,8 @@ export default {
   max-width: 100%;
   height: auto;
   border-radius: 2rem;
+}
+.content {
+  padding: 2rem;
 }
 </style>
