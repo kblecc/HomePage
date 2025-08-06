2322b48235458cd4b9fe361e98512ed6fca70e79
kble <github@kble.cc>
2025-08-06 08:14:26 +0800

    Add: Hear i am Website

---
+++ src/components/portfolio/HearIamWebsite.vue
@@ -0,0 +1,40 @@
+<template>
+  <img
+    src="https://raw.githubusercontent.com/kblecc/HomePage/refs/heads/2025/src/assets/portfolio/HearIamWebsite/Banner.jpg"
+    class="k-card-img"
+    alt="Scenic nature landscape with mountains, lake, and forest during golden hour"
+  />
+  <div class="content">
+    <!-- <h3 class="mb-1 fw-bold">Grant Nexus</h3>
+    <div class="k-tags-container">
+      <span class="badge rounded-pill text-bg-primary">Design</span>
+      <span class="badge rounded-pill text-bg-primary">UI</span>
+    </div>
+
+    <p class="mb-0 text-muted">
+      Hear i am is a mobie application for music therapy combined with mental care
+    </p> -->
+
+    <img
+      class="k-content-image"
+      src="https://raw.githubusercontent.com/kblecc/HomePage/refs/heads/2025/src/assets/portfolio/HearIamWebsite/Screenshot.jpg"
+    />
+  </div>
+</template>
+<style scoped>
+.k-card-img {
+  max-width: 100%;
+  height: auto;
+  border-radius: 2rem;
+}
+
+.content {
+  padding: 2rem;
+}
+
+.k-content-image {
+  width: 100%;
+  height: auto;
+  border-radius: 1rem;
+}
+</style>
